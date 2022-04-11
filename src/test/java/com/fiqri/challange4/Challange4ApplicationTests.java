973985d7cfb58ef25c09056f3355dd20885e8f6b
Fiqri Maulana Arokhman <fiqrimaulanaarokhman@gmail.com>
2022-04-11 11:35:32 +0700

    initial commit

---
+++ src/test/java/com/fiqri/challange4/Challange4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiqri.challange4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Challange4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
